647c71f89a3d9314ece570b0f9f5eda7e67a26a5
roybr03 <roybr2004@gmail.com>
2026-02-08 21:41:18 +0600

    Assignment done

--- q1.js
+++ q1.js
@@ -1,15 +1,8 @@
-
-/*git remote add origin https://github.com/roybr03/JSassignment.git
-git branch -M main
-git push -u origin main*/
 function  newPrice(currentPrice,discount) {
-    if (typeof(currentPrice) !== "number" || typeof(discount) !== "number") {
+    if (typeof currentPrice !== "number" || typeof discount !== "number") {
         return "Invalid";
     }
 
     let results = (currentPrice - (currentPrice*(discount/100)));
-    return results.toFixed(3);
+    return Number(results.toFixed(3));
 }
-
-let p = newPrice(500, "5");
-console.log(p);
